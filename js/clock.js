9c7183bcc3eeeed48572e5bf0f1e4256b479def0
Kaan <108834287+AxciomTM@users.noreply.github.com>
2023-05-14 17:25:54 +0300

    Create clock.js

---
+++ js/clock.js
@@ -0,0 +1,23 @@
+//isim bilgisi:
+
+let name=prompt("Lütfen isminizi giriniz:");
+let myName=document.querySelector("#myName");
+myName.innerHTML = ` ${name}`;
+
+function time(){
+    
+    const date=new Date;
+
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    
+    
+    let day =["Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"];
+    let dayName = day[date.getDay()]
+    
+    let clock=document.querySelector("#myClock");
+    clock.innerHTML = `${hour}:${minute}:${second} ${dayName}`;
+}
+
+setInterval(time,1000);
